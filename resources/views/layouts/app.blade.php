ed2f6ab0a5b91cbef70de4cdb3607ddd8471cadf
Naquiya Asghar <naquiya.asghar@lokal.pk>
2023-12-13 11:49:35 +0500

    Installing tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
     <title>Posty</title>
+    
 </head>
 <body>
+    
     <!-- this code is to inject the content -->
     @yield('content')
+    
 </body>
 </html>
